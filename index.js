b37d966b98d99fb022f9c07c9c6676ce6a85076d
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-03-23 04:52:33 +0300

    first commit on arrays

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+function findMatching(drivers, query) {
+return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+
+}
+
+function fuzzyMatch(drivers,query) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+}
+
+function matchName(drivers,query) {
+    return drivers.filter(driver => driver.name===query);
+}
+
+const drivers = ["Alice", "Bob", "Charlie", "alice"];
+const driverObjects = [
+  { name: "Alice", hometown: "New York" },
+  { name: "Bob", hometown: "Los Angeles" },
+  { name: "Charlie", hometown: "Chicago" }
+];
+
+console.log(findMatching(drivers, "alice")); 
+console.log(fuzzyMatch(drivers, "Al")); 
+console.log(matchName(driverObjects, "Bob")); 
